bba61ea8906b08da2bfadeca55592a6b548aa452
thisgf <274397645@qq.com>
2023-03-03 10:40:56 +0800

    Update main.go

--- cmd/query_benchmarker_influxdb/main.go
+++ cmd/query_benchmarker_influxdb/main.go
@@ -19,9 +19,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/influxdata/influxdb-comparisons/bulk_query"
-	"github.com/influxdata/influxdb-comparisons/bulk_query/http"
-	"github.com/influxdata/influxdb-comparisons/util/report"
+	"github.com/thisgf/influxdb-comparisons/bulk_query"
+	"github.com/thisgf/influxdb-comparisons/bulk_query/http"
+	"github.com/thisgf/influxdb-comparisons/util/report"
 )
 
 // Program option vars:
